4a586f33a59611d1704b7face41201e94f65d4bd
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2020-02-03 22:01:00 +0000

    Hide client secret in auth.sh

--- auth.sh
+++ auth.sh
@@ -3,11 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "AUTH.SH: OAuth 2.0 credentials: Fetch..."
-echo "Enter 'OAuth 2.0 Client ID' (clientId):"
-read clientId
-
-echo "Enter 'Client Secret' (clientSecret):"
-read clientSecret
+read -p "Enter 'OAuth 2.0 Client ID': " clientId
+stty -echo
+read -p "Enter 'Client Secret': " clientSecret; echo
+stty echo
 
 echo "AUTH.SH: Generate access tokens: Opening web browser..."
 cd "${DIR}/python"
